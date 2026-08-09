6cb1fcc80213af7a2e214da8a44b714d6969d115
swappys <swapnilvernekar04@gmail.com>
2026-08-09 11:44:58 +0100

    Removing business logic from controller
    Removing business logic from controller to add into service layer.

--- calendarService/calendar/src/main/java/com/calendarService/calendar/controller/CalendarController.java
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/controller/CalendarController.java
@@ -5,32 +5,25 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.calendarService.calendar.service.CalendarService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/calendar")
 public class CalendarController {
     
+    final CalendarService calendarService;
+
+    CalendarController(CalendarService calendarService) {
+        this.calendarService = calendarService;
+    }
 
     @GetMapping()
     public List<Map<String, String>> getCalendar(){
-        return List.of(
-            Map.of(
-                "Title","Team Meeting",
-                "Time","10:00 Monday"
-            ),
-            Map.of(
-                "Title","Project Demo",
-                "Time","15:00 Wednesday"
-            ),
-            Map.of(
-                "Title","Project Meeting",
-                "Time","12:00 Wednesday"
-            ),
-            Map.of(
-                "Title","Management Meeting",
-                "Time","13:00 Friday"
-            )
-        );
+            return calendarService.fetchEvents();
+    
     }
 }
