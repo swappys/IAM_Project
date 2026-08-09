7d19649b2e51fc4527e316990ace73ade0476acd
swappys <swapnilvernekar04@gmail.com>
2026-08-09 13:33:21 +0100

    Auto insert data during startup
    Config for Auto insert data during startup

---
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package com.calendarService.calendar.config;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.calendarService.calendar.models.EventModel;
+import com.calendarService.calendar.repository.EventRepository;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initDatabase(EventRepository eventRepository){
+        return args->{
+            eventRepository.save(
+                EventModel.builder()
+                            .title("Team Meeting")
+                            .time(LocalDateTime.of(2026, 8, 10, 10, 0))
+                            .build()
+            );
+            eventRepository.save(
+                EventModel.builder()
+                            .title("Management Meeting")
+                            .time(LocalDateTime.of(2026, 8, 11, 10, 0))
+                            .build()
+            );
+        };
+    }
+}
