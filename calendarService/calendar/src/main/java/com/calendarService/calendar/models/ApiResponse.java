d3611491f41f5dbab8e8b059a6040052c71d7052
swappys <swapnilvernekar04@gmail.com>
2026-08-09 15:35:24 +0100

    Create ApiResponse.java

---
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/models/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.calendarService.calendar.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+    private Boolean error;
+}
