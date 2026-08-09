0896034e257bf76b4dd93e3c9ea6fc2cc9e0181c
swappys <swapnilvernekar04@gmail.com>
2026-08-09 13:32:54 +0100

    Add Event Model and EventRepo
    Add Event Model and EventRepo to store events

---
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/models/EventModel.java
@@ -0,0 +1,32 @@
+package com.calendarService.calendar.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EventModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name="title")
+    public String title;
+
+    @Column(name = "time")
+    public LocalDateTime time;
+}
---
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.calendarService.calendar.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.calendarService.calendar.models.EventModel;
+
+public interface EventRepository extends JpaRepository<EventModel, Long>{
+    
+}
