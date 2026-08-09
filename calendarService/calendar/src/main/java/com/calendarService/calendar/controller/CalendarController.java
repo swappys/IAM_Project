4f60b264ef341acd0d2100f6580b9973d800a662
swappys <swapnilvernekar04@gmail.com>
2026-08-09 11:47:16 +0100

    removing unwanted imports
    removing unwanted imports

--- calendarService/calendar/src/main/java/com/calendarService/calendar/controller/CalendarController.java
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/controller/CalendarController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.calendarService.calendar.service.CalendarService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
