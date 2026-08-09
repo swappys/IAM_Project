766316807605cbb7bc11fa52e4c7aeb82a2b516c
swappys <swapnilvernekar04@gmail.com>
2026-08-09 15:36:09 +0100

    update to Calender Service
    update to calender Service to use repository to get and push data

--- calendarService/calendar/src/main/java/com/calendarService/calendar/service/CalendarService.java
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/service/CalendarService.java
@@ -3,29 +3,48 @@ package com.calendarService.calendar.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.calendarService.calendar.models.ApiResponse;
+import com.calendarService.calendar.models.EventModel;
+import com.calendarService.calendar.models.RequestModel;
+import com.calendarService.calendar.repository.EventRepository;
 
 @Service
 public class CalendarService {
-    
-    public List<Map<String,String>> fetchEvents(){
-        return List.of(
-            Map.of(
-                "Title","Team Meeting",
-                "Time","10:00 Monday"
-            ),
-            Map.of(
-                "Title","Project Demo",
-                "Time","15:00 Wednesday"
-            ),
-            Map.of(
-                "Title","Project Meeting",
-                "Time","12:00 Wednesday"
-            ),
-            Map.of(
-                "Title","Management Meeting",
-                "Time","13:00 Friday"
-            )
-        );
+
+    final EventRepository eventRepository;
+
+    CalendarService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<EventModel> fetchEvents(){
+        return eventRepository.findAll();
+    }
+
+    public ResponseEntity<ApiResponse> addEvent(RequestModel model){
+        try{
+            EventModel eventModel = EventModel.builder()
+                                                .title(model.title)
+                                                .time(model.time)
+                                                .build();
+            EventModel savedEvent = eventRepository.save(eventModel);
+            ApiResponse response = ApiResponse.builder()
+                                            .message("Event saved successfully")
+                                            .error(false)
+                                            .data(savedEvent)
+                                            .build();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }catch(Exception ex){
+            ApiResponse response = ApiResponse.builder()
+                                            .message("Failed to save event")
+                                            .error(true)
+                                            .build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
