14ad98d68243e7ae196b96719a824e01feaa3eec
swappys <swapnilvernekar04@gmail.com>
2026-08-09 15:35:29 +0100

    Create RequestModel.java

---
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/models/RequestModel.java
@@ -0,0 +1,23 @@
+package com.calendarService.calendar.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestModel {
+
+    public String title;
+
+    public LocalDateTime time;
+}
