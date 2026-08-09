f0b9d9b3da36a1bda968437a7ed187ff163a2ccc
swappys <swapnilvernekar04@gmail.com>
2026-08-09 15:37:52 +0100

    Update EventRepository.java

--- calendarService/calendar/src/main/java/com/calendarService/calendar/repository/EventRepository.java
+++ calendarService/calendar/src/main/java/com/calendarService/calendar/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.calendarService.calendar.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.calendarService.calendar.models.EventModel;
 
 public interface EventRepository extends JpaRepository<EventModel, Long>{
