3777fe61c32e51b90ca46f099736a4836b760b56
swappys <swapnilvernekar04@gmail.com>
2026-08-10 16:34:11 +0100

    removed traces that would expose tokens
    removed traces that would expose tokens

--- frontend-app/frontend-app/src/auth/AuthProvider.js
+++ frontend-app/frontend-app/src/auth/AuthProvider.js
@@ -33,7 +33,7 @@ export function AuthProvider({ children }) {
 
                     return;
                 }
-                console.log("Token:", keycloak.tokenParsed);
+                console.log("ACR:", keycloak.tokenParsed?.acr);
                 setAuthenticated(true);
 
                 setUser({
